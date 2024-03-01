730f051051ec67fde30c2aa62d060aa2c3861de8
Shahidul-Tanim <115738942+Shahidul-Tanim@users.noreply.github.com>
2024-03-02 00:41:48 +0600

    Class 3

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,16 +12,40 @@ class PostController extends Controller
     }
 
     function allPosts(){
-        return view('allPosts') ;
+
+        $posts = Post::latest()->paginate(5);
+
+        return view('allPosts', compact('posts')) ;
     }
 
     function storePost(Request $request){
 
         // *VALIDATION
         $request->validate([
-            'title' => 'required|min:10',
-            'detail' => 'required|max:15'
+            'title' => 'required|min:15',
+            'detail' => 'required|max:150'
         ]);
+
+        //  *STORE DATA
+        $post = new Post();
+        $post->title = $request->title;
+        $post->detail = $request->detail;
+        $post->author = $request->author;
+        $post->save();
+
+        return back()->with('success', 'The Post Has Submitted Successfully');
       
+    }
+
+    function deletePost($id){
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return back();
+    }
+
+    function EditData($id){
+        $post = Post::find($id);
+        // dd($post);
+        return view('editPost', compact('post'));
     }
 }

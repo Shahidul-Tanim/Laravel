052ecfa6a5b4d89c4f28ddc853879ffc83565d58
Shahidul-Tanim <115738942+Shahidul-Tanim@users.noreply.github.com>
2024-03-02 00:43:08 +0600

    Class 3

--- example-app/resources/views/allPosts.blade.php
+++ example-app/resources/views/allPosts.blade.php
@@ -5,7 +5,37 @@
 @section('content')
     <section>
       <div class="container">
-        <h1>hello</h1>
+        
+        <div class="table-responsive mt-5 text-center">
+          <table class="table">
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Detail</th>
+              <th>Author</th>
+              <th></th>
+            </tr>
+
+            @foreach ($posts as $key=> $post )
+                
+             <tr>
+               <td>{{ $posts->firstItem() + $key }}</td>
+               <td>{{ $post->title }}</td>
+               <td>{{ str($post->detail)->substr(0, 16)->lower().'...' }}</td>
+               <td>{{ $post->author }}</td>
+               <td>
+                 <div class="btn-group">
+                  <a class="btn btn-sm btn-primary" href="{{ route('edit', $post->id)}}">Edit</a>
+                  <a class="btn btn-sm btn-danger" href="{{ route('delete', $post->id)}}">Delete</a>
+                </div>
+               </td>
+             </tr>
+
+            @endforeach
+          </table>
+          {{ $posts->links() }}
+        </div>
+
       </div>
     </section>
 @endsection()
